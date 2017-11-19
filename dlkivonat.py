655c519377bcc5670720ebfebbb5ca07b4948021
Your Name <you@example.com>
2017-11-19 20:14:32 +0000

    electra change

--- dlkivonat.py
+++ dlkivonat.py
@@ -6,9 +6,9 @@ from datetime import datetime, timedelta, date
 from bs4 import BeautifulSoup
 import re
 
-url1 = 'https://ibank.takarekpont.hu/eib/loginpage.hu.html/'
-url = 'https://ibank.takarekpont.hu/eib/login'
-year = 2016
+url1 = 'https://electra7.takinfo.hu/eib_ib_S1/loginpage.hu.html'
+url = 'https://electra7.takinfo.hu/eib_ib_S1/login'
+year = 2017
 user = ''
 passwd = ''
 
@@ -34,7 +34,7 @@ data = "pageid={}&lang=hu&cgcode=&group=&user={}&uid={}&pwd={}&B1=Bejelentkez%E9
 values = data2dict(data)
 print values
 s = r.post(url, data=values)
-s = r.get('https://ibank.takarekpont.hu/eib/getstatementform/pdf')
+s = r.get('https://electra7.takinfo.hu/eib_ib_S1/getstatementform/pdf')
 
 
 for i in range(1,13):
@@ -47,25 +47,51 @@ for i in range(1,13):
         fromdate = str(date(year,i,01)).replace('-','.')
         todate = str(date(year,i+1,01) - timedelta(days=1)).replace('-','.')
         print fromdate, todate
-    data = "pageid={}&datelist=ok&type=PDF&from={}&to={}&OK=Rendben".format(pageid, fromdate, todate)
-
-    url = 'https://ibank.takarekpont.hu/eib/getstatement'
-    s = r.post(url, data=data2dict(data))
-
-    pattern = 'cO3\(\'(.*?)\''
-    matches = re.findall(pattern, s.text)
+    data = {
+        'pageid': pageid,
+        'datelist': 'ok',
+        'type': "PDF",
+        'from': fromdate,
+        'to': todate,
+        'OK': 'Rendben'
+    }
+    url = 'https://electra7.takinfo.hu/eib_ib_S1/getstatement'
+    s = r.post(url, data=data)
+    soup = BeautifulSoup(s.text, 'lxml')
+    #print soup.find_all('tr')
+    matches = soup.find_all('tr', {"id" : re.compile('etktable_nodatelist.*')})
+    print matches
+    # continue
+    if not matches:
+        continue
     for match in matches:
-        tdate = str(match)
+        print match
+        tdate = re.findall('date\',\'(.*?)\'', str(match))
+        tdate = str(tdate[0])
+        print tdate
         tdatefield = str(date(int(tdate[:4]), int(tdate[4:6]), int(tdate[6:8]))).replace('-','.')
         fromdate = fromdate.replace('.','')
         todate = todate.replace('.','')
         print tdate, tdatefield
-        data = "pageid={}&datelist_back=ok&type=PDF&from={}&to={}&OK=Rendben&date={}&date_field={}".format(pageid, fromdate, todate, tdate, tdatefield)
+        data =  {
+            'pageid': pageid,
+            'type': 'PDF',
+            'from': fromdate,
+            'to': todate,
+            'OK': 'Rendben',
+            'date': tdate,
+            'sourcefile': '6665.pdfstmcat.xml',
+            'nodatelist': None,
+            'acc': None,
+            'etk_table_nodatelist_itemcount': len(matches)
+        }
+
         print data
-        s = r.post('https://ibank.takarekpont.hu/eib/getstatement', data=data2dict(data))
-        dlurl = '/eib/getpdfstatement/5170020310012772/statement_{}_5170020310012772.pdf'.format(tdatefield)
-        s = r.get('https://ibank.takarekpont.hu' + dlurl, stream=True)
-        local_filename = 'statement_{}_5170020310012772.pdf'.format(tdatefield)
+        s = r.post('https://electra7.takinfo.hu/eib_ib_S1/getstatement', data=data)
+        dlurl = '/eib_ib_S1/getpdfstatement/5170020310012772/statement_{}_5170020310012772.pdf'.format(tdate)
+        s = r.get('https://electra7.takinfo.hu' + dlurl, stream=True)
+        print dlurl
+        local_filename = 'statement_{}_5170020310012772.pdf'.format(tdate)
         with open(local_filename, 'wb') as f:
             for chunk in s.iter_content(chunk_size=1024):
                 if chunk: # filter out keep-alive new chunks
